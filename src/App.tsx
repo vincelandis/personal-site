0ddb15674aad6a8cdd6b34696519d84f938fe36a
Vince Landis <vincelandis@gmail.com>
2025-10-22 10:33:44 -0400

    clean up comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import WishList from './WishList/WishList'
 import legoWishes from './data/wishlist-lego.json'
@@ -11,14 +9,6 @@ function App() {
 
   return (
     <>
-      {/* <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a> */}
-      {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      {/* <div>
-      </div> */}
       <h1>Vince Wishlist</h1>
       <div className='card'>
         <button className={`button-left ${mode === 'standard' ? 'selected' : ''}`}
